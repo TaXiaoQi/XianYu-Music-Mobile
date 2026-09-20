cb58303a0d217b70fbc99109b5c6ee0872f8d824
小奇 <2784213157@qq.com>
2026-09-20 23:40:30 +0800

    Revert "fix(移动端): 划掉多任务即停止播放 - AudioService.onTaskRemoved 显式 stopSelf 结束服务移除通知(鸿蒙4兼容层对前台媒体服务默认保活导致划掉不停), Dart onTaskRemoved 同步暂停"
    This reverts commit c20701d1cdfc91ff50cc8787e2c777b584becbd2.

--- lib/src/player/player_provider.dart
+++ lib/src/player/player_provider.dart
@@ -252,11 +252,6 @@ class XianYuAudioHandler extends as_pkg.BaseAudioHandler with as_pkg.SeekHandler
 
   @override
   Future<void> stop() => _notifier?.pauseFromSystem() ?? Future.value();
-
-  @override
-  Future<void> onTaskRemoved() async {
-    await stop();
-  }
 }
 
 class QueueItem {
